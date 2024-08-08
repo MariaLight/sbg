5a64fa0047cf8e52b7bd3b92b6750a81134f5e7c
MariaLight <61028743+MariaLight@users.noreply.github.com>
2024-08-08 19:36:18 +1200

    add desktop without animation

---
+++ assets/resources/feedиack-swiper.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.feedback-swiper', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+
+    pagination: {
+        el: '.feedback .swiper-pagination',
+        dynamicBullets: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.feedback .swiper-button-next',
+        prevEl: '.feedback .swiper-button-prev',
+    },
+
+});
